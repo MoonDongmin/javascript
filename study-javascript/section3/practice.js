80efee8608f2ef68d12486a997a611cd90da3a4a
MoonDongmin <20191836@o365.hanbat.ac.kr>
2024-11-28 17:32:37 +0900

    section03. 제어문

---
+++ study-javascript/section3/practice.js
@@ -0,0 +1,13 @@
+const score = 85;
+
+if (score >= 90) {
+  console.log('A');
+} else if (score >= 80 && score < 90) {
+  console.log('B');
+}
+
+let count = 5;
+while (count) {
+  console.log(count ** count);
+  count--;
+}
